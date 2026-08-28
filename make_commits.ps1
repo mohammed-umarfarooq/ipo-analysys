41aa588ade6fbe4367d2b817611e21f3a0401699
mohammed-umarfarooq <mohammedumarfarooq21@gmail.com>
2026-08-28 11:01:27 +0530

    Remove contribution logs and script

--- make_commits.ps1
+++
@@ -1,8 +0,0 @@
-for ($i = 1; $i -le 20; $i++) {
-    $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    Add-Content -Path "contribution_log2.txt" -Value "Step commit $i at $date"
-    git add contribution_log2.txt
-    git commit -m "Step commit $i"
-    git push origin master
-    Start-Sleep -Seconds 2
-}
